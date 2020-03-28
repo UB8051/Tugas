804791263d64f35c802fc514aaa814661e8342fb
Muhammad Rifqi Firmansyah <muhammadrifqiif.04@gmail.com>
2020-03-28 13:16:06 +0700

    all

--- app/src/main/java/com/example/tugaspam/MainActivity.java
+++ app/src/main/java/com/example/tugaspam/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.tugaspam;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -20,10 +26,13 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.Calendar;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
-    String a,b;
+    String a, b, jns;
     Button Daftar, Keluar;
-    RadioButton lk, pr;
+    RadioButton lk, pr, islam, katolik, buddha, alkep, kristen, hindu, konghucu;
     TextInputEditText Namadpn, Namablkng, Tempatlhr, Tgllhr, Alamat, Telepon , Email, Pass, Inpass;
     TextView data1, data2, data3, data4, data5, data6, data7, data8;
     RadioGroup Jns, mFirstGroup, mSecondGroup, mThirdGroup,mFourthGroup;
@@ -31,15 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private int mCheckedId = R.id.type1;
     LayoutInflater inflater;
     View dialogView;
-
-//    private void kosong(){
-//        txt_namadpn.findViewById(R.id.data1);
-//        txt_namablkng.findViewById(R.id.data2);
-//        txt_ttl.findViewById(R.id.data3);
-//        txt_almt.findViewById(R.id.data4);
-//        txt_telp.findViewById(R.id.data7);
-//        txt_mail.findViewById(R.id.data8);
-//    }
+    int tahun, bulan, hari;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,74 +54,26 @@ public class MainActivity extends AppCompatActivity {
         Tempatlhr = findViewById(R.id.input_tempat_lahir);
         Tgllhr = findViewById(R.id.input_tanggal_lahir);
         Alamat = findViewById(R.id.input_alamat);
-        Jns = findViewById(R.id.Jns);
         Telepon = findViewById(R.id.input_telepon);
         Email = findViewById(R.id.input_email);
         Pass = findViewById(R.id.input_password);
         Inpass = findViewById(R.id.input_masukan_password);
 
+        Jns =  (RadioGroup) findViewById(R.id.Jns);
         mFirstGroup = (RadioGroup) findViewById(R.id.first_group);
         mSecondGroup = (RadioGroup) findViewById(R.id.second_group);
         mThirdGroup = (RadioGroup) findViewById(R.id.third_group);
         mFourthGroup = (RadioGroup) findViewById(R.id.fourth_group);
 
-        lk = (RadioButton) findViewById(R.id.lk);
-        pr = (RadioButton) findViewById(R.id.pr);
-        mFirstGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId != -1 && isChecking) {
-                    isChecking = false;
-                    mSecondGroup.clearCheck();
-                    mThirdGroup.clearCheck();
-                    mFourthGroup.clearCheck();
-                    mCheckedId = checkedId;
-                }
-                isChecking = true;
-            }
-        });
-
-        mSecondGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId != -1 && isChecking) {
-                    isChecking = false;
-                    mFirstGroup.clearCheck();
-                    mThirdGroup.clearCheck();
-                    mFourthGroup.clearCheck();
-                    mCheckedId = checkedId;
-                }
-                isChecking = true;
-            }
-        });
-
-        mThirdGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId != -1 && isChecking) {
-                    isChecking = false;
-                    mFirstGroup.clearCheck();
-                    mSecondGroup.clearCheck();
-                    mFourthGroup.clearCheck();
-                    mCheckedId = checkedId;
-                }
-                isChecking = true;
-            }
-        });
-
-        mFourthGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId != -1 && isChecking) {
-                    isChecking = false;
-                    mFirstGroup.clearCheck();
-                    mSecondGroup.clearCheck();
-                    mThirdGroup.clearCheck();
-                    mCheckedId = checkedId;
-                }
-                isChecking = true;
-            }
-        });
+        lk = findViewById(R.id.lk);
+        pr = findViewById(R.id.pr);
+        islam = findViewById(R.id.type1);
+        kristen = findViewById(R.id.type2);
+        katolik = findViewById(R.id.type3);
+        hindu = findViewById(R.id.type4);
+        buddha = findViewById(R.id.type5);
+        konghucu = findViewById(R.id.type6);
+        alkep = findViewById(R.id.type7);
 
         Keluar = (Button) findViewById(R.id.button1);
         Keluar.setOnClickListener(new View.OnClickListener() {
@@ -129,6 +82,24 @@ public class MainActivity extends AppCompatActivity {
                 moveTaskToBack(true);
                 finish();
                 System.exit(0);
+            }
+        });
+
+        Email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!Email.getText().toString().trim()
+                        .matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
+                    Email.setError("Format Email Salah");
+                }
             }
         });
 
@@ -167,8 +138,60 @@ public class MainActivity extends AppCompatActivity {
                 } else AlertDialog();
             }
 
+            public boolean pattern(String pass) {
+                String patt = "^(?=.*[0-9])" +
+                        "(?=.*[a-z])" +
+                        "(?=.*[A-Z])" +
+                        "(?=.*[@#$%^&+=])" +
+                        "(?=\\S+$).{4,}" +
+                        "$";
+                return Pattern.compile("^" +
+                        "(?=.*[0-9])" +
+                        "(?=.*[a-z])" +
+                        "(?=.*[A-Z])" +
+                        "(?=.*[@#$%^&+=])" +
+                        "(?=\\S+$).{4,}" +
+                        "$").matcher(pass).matches();
+            }
+            public void isValidPassword(){
+                String err = "Panjang password minimal adalah 8 karakter dengan angka, huruf " +
+                        "besar, huruf kecil, dan spesial karakter (seperti: @,&,#,?)";
+                if (TextUtils.isEmpty(Pass.getText().toString().trim()) ||
+                        Pass.getText().toString().trim().length() < 8) {
+                    Pass.setError(err);
+                } else if (!pattern(Pass.getText().toString().trim())) {
+                    Pass.setError(err);
+                } else {
+                    if (!Pass.getText().toString().trim().equals(
+                            Inpass.getText().toString().trim())) {
+                        Inpass.setError("Password harus sama");
+                    }
+                }
+            }
+        });
+        Tgllhr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePick();
+            }
         });
 
+    }
+    public void datePick() {
+        Calendar d = Calendar.getInstance();
+        tahun = d.get(Calendar.YEAR);
+        bulan = d.get(Calendar.MONTH);
+        hari = d.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dPD = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int _tahun, int _bulan, int _hari) {
+                        Tgllhr.setText(_hari + "-" + (_bulan + 1) + "-" + _tahun);
+                    }
+                }, tahun, bulan, hari
+        );
+
+        dPD.show();
     }
     public void AlertDialog(){
         AlertDialog.Builder ab = new AlertDialog.Builder(MainActivity.this);
@@ -186,7 +209,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void CustomDialog() {
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        final AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
         inflater = getLayoutInflater();
         dialogView = inflater.inflate(R.layout.detail_pendaftran, null);
         mBuilder.setCancelable(true);
@@ -207,11 +230,18 @@ public class MainActivity extends AppCompatActivity {
         data2.setText(Namablkng.getText());
         data3.setText(Tempatlhr.getText() + "," + Tgllhr.getText());
         data4.setText(Alamat.getText());
+        data5.setText(jns);
         data6.setText(a);
         data7.setText(Telepon.getText());
         data8.setText(Email.getText());
 
-        mBuilder.setPositiveButton("SELESAI", null);
+        mBuilder.setPositiveButton("SELESAI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pendaftaran Selesai",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
         mBuilder.setNegativeButton("KELLUAR", new DialogInterface.OnClickListener() {
             @Override
@@ -224,69 +254,80 @@ public class MainActivity extends AppCompatActivity {
 
 
         mBuilder.show();
-    }
+    }public void ChooseRB(View v) {
+        boolean checked = ((RadioButton) v).isChecked();
 
-    public void JenisKlmn (View view) {
-
-        Boolean checked = ((RadioButton) view).isChecked();
-
-        switch (view.getId()) {
+        switch (v.getId()) {
             case R.id.lk:
-                if (checked)
-                    a = "Laki-Laki";
-                mSecondGroup.clearCheck();
-                mThirdGroup.clearCheck();
-                mFourthGroup.clearCheck();
+                if (checked) {
+                    jns = "Laki-Laki";
+                }
                 break;
-
             case R.id.pr:
-                if (checked)
-                    a = "Perempuan";
-                mFirstGroup.clearCheck();
-                mThirdGroup.clearCheck();
-                mFourthGroup.clearCheck();
+                if (checked) {
+                    jns = "Perempuan";
+                }
+                break;
+            case R.id.type1:
+                if (checked) {
+                    a = "Islam";
+                    mSecondGroup.clearCheck();
+                    mThirdGroup.clearCheck();
+                    mFourthGroup.clearCheck();
+                }
+                break;
+            case R.id.type2:
+                if (checked) {
+                    a = "Kristen";
+                    mSecondGroup.clearCheck();
+                    mThirdGroup.clearCheck();
+                    mFourthGroup.clearCheck();
+                }
+                break;
+            case R.id.type3:
+                if (checked) {
+                    a = "Katolik";
+                    mFirstGroup.clearCheck();
+                    mThirdGroup.clearCheck();
+                    mFourthGroup.clearCheck();
+                }
+                break;
+            case R.id.type4:
+                if (checked) {
+                    a = "Hindu";
+                    mFirstGroup.clearCheck();
+                    mThirdGroup.clearCheck();
+                    mFourthGroup.clearCheck();
+                }
+                break;
+            case R.id.type5:
+                if (checked) {
+                    a = "Buddha";
+                    mFirstGroup.clearCheck();
+                    mSecondGroup.clearCheck();
+                    mFourthGroup.clearCheck();
+                }
+                break;
+            case R.id.type6:
+                if (checked) {
+                    a = "Konghucu";
+                    mFirstGroup.clearCheck();
+                    mSecondGroup.clearCheck();
+                    mFourthGroup.clearCheck();
+                }
+                break;
+            case R.id.type7:
+                if (checked) {
+                    a = "Aliran Kepercayaan";
+                    mFirstGroup.clearCheck();
+                    mSecondGroup.clearCheck();
+                    mThirdGroup.clearCheck();
+                }
+                break;
+            default:
                 break;
         }
     }
 
-//    public void Agama(View view) {
-//
-//        Boolean checked = ((RadioButton) view).isChecked();
-//
-//        switch (view.getId()) {
-//            case R.id.first_group:
-//                if (checked)
-//                    Agama = "Islam";
-//                mSecondGroup.clearCheck();
-//                mThirdGroup.clearCheck();
-//                mFourthGroup.clearCheck();
-//                break;
-//
-//            case R.id.second_group:
-//                if (checked)
-//                    Agama = "Katolik";
-//                mFirstGroup.clearCheck();
-//                mThirdGroup.clearCheck();
-//                mFourthGroup.clearCheck();
-//                break;
-//
-//            case R.id.third_group:
-//                if (checked)
-//                    Agama = "Buddha";
-//                mFirstGroup.clearCheck();
-//                mSecondGroup.clearCheck();
-//                mFourthGroup.clearCheck();
-//                break;
-//
-//            case R.id.fourth_group:
-//                if (checked)
-//                    Agama = "Aliran Kepercayaan";
-//                mFirstGroup.clearCheck();
-//                mThirdGroup.clearCheck();
-//                mSecondGroup.clearCheck();
-//                break;
-//        }
-//        isChecking = true;
-//    }
 }
 
